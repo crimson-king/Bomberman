c7190605020d03ab52e14a3c8e481103043042d3
aruziell <aruziell@gmail.com>
2015-04-22 22:05:30 +0200

    add state module
    this contains following code:
    State interface
    StateManager class with module instance
    StatedGameHandler for games with states
    
    sample usage when running as a script

---
+++ pybomberman/state.py
@@ -0,0 +1,111 @@
+from pybomberman import GameHandler, Game
+
+
+class State(GameHandler):
+    def start(self):
+        pass
+
+    def pause(self):
+        pass
+
+    def resume(self):
+        pass
+
+    def destroy(self):
+        pass
+
+
+class StateManager(GameHandler):
+    def __init__(self):
+        self._states = []
+
+    def push(self, state):
+        print('pushing state', state)
+        if self._states:
+            self._states[-1].pause()
+
+        state.start()
+        self._states.append(state)
+        state.resume()
+
+    def pop(self):
+        state = self._states[-1]
+        print('popping state', state)
+        state.pause()
+        self._states.pop()
+        state.destroy()
+
+        if self._states:
+            self._states[-1].resume()
+
+        return state
+
+    def handle_input(self, event):
+        self._states[-1].handle_input(event)
+
+    def handle_update(self, dt):
+        self._states[-1].handle_update(dt)
+
+    def handle_draw(self, canvas):
+        self._states[-1].handle_draw(canvas)
+
+
+manager = StateManager()
+
+
+class StatedGameHandler(GameHandler):
+    def handle_input(self, event):
+        manager.handle_input(event)
+
+    def handle_update(self, dt):
+        manager.handle_update(dt)
+
+    def handle_draw(self, canvas):
+        manager.handle_draw(canvas)
+
+
+if __name__ == '__main__':
+    class SampleGameHandler(StatedGameHandler):
+        max_state_time = 2000
+
+        def __init__(self):
+            manager.push(SampleGameHandler.RedState())
+
+        class TimedColorState(State):
+            def __init__(self, color):
+                self.time = 0
+                self.color = color
+
+            def handle_input(self, event):
+                pass
+
+            def handle_update(self, dt):
+                self.time += dt
+                if self.time >= SampleGameHandler.max_state_time:
+                    self.state_end()
+
+            def handle_draw(self, canvas):
+                canvas.fill(self.color)
+
+            def state_end(self):
+                raise NotImplementedError
+
+        class YellowState(TimedColorState):
+            def __init__(self):
+                super().__init__(color=(0xff, 0xff, 0))
+
+            def state_end(self):
+                manager.pop()
+
+        class RedState(TimedColorState):
+            def __init__(self):
+                super().__init__(color=(0xff, 0, 0))
+
+            def resume(self):
+                self.time = 0
+
+            def state_end(self):
+                manager.push(SampleGameHandler.YellowState())
+
+    print('running state demo..')
+    Game(handler=SampleGameHandler()).start()
