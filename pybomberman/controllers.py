520472dd0c2a05c7c7c52ef6fc25435f25601c7c
aruziell <aruziell@gmail.com>
2015-05-04 14:42:38 +0200

    add controllers module with HumanController

---
+++ pybomberman/controllers.py
@@ -0,0 +1,25 @@
+from framework.input import NormalAction, InitialAction
+from pybomberman.objects import Player
+
+
+class Controller:
+    def update(self, dt):
+        pass
+
+
+class HumanController(Controller):
+    def __init__(self, player: Player):
+        self.player = player
+        self.action_action = InitialAction()
+        self.action_up = NormalAction()
+        self.action_down = NormalAction()
+        self.action_left = NormalAction()
+        self.action_right = NormalAction()
+
+    def update(self, dt):
+        dx, dy = 0, 0
+
+        dx += self.action_right.active() - self.action_left.active()
+        dy -= self.action_up.active() - self.action_down.active()
+
+        self.player.rect.move_ip(dx, dy)
