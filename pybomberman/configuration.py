9f01806f3c7f2255ffff2749f3791eb6dea01f46
aruziell <aruziell@gmail.com>
2015-05-04 14:43:35 +0200

    add PlayerKeyConfiguration to Configuration

--- pybomberman/configuration.py
+++ pybomberman/configuration.py
@@ -1,7 +1,29 @@
+import pygame
+
+
+class PlayerKeyConfiguration:
+    def __init__(self,
+                 action=pygame.K_SPACE,
+                 up=pygame.K_UP,
+                 down=pygame.K_DOWN,
+                 left=pygame.K_LEFT,
+                 right=pygame.K_RIGHT):
+        self.action = action
+        self.up = up
+        self.down = down
+        self.left = left
+        self.right = right
+
+
 class Configuration:
     players = 2
     # resolution = (...)
     # key bindings for each player = <tu po jakiejs liscie po 5 klawiszy>
+
+    def __init__(self):
+        self.player_key_configs = [PlayerKeyConfiguration()
+                                   for i in range(self.players)]
+
 
 configuration = Configuration()
 """class _Configuration:
