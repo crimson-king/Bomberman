f07661a154201bf80eccf3d160248ba1314baeb2
aruziell <aruziell@gmail.com>
2015-05-04 15:35:58 +0200

    add PPM constant and Wall object

--- pybomberman/objects.py
+++ pybomberman/objects.py
@@ -1,10 +1,21 @@
 import pygame
 from pygame.sprite import Sprite
 
+from pybomberman import PPM
+
 
 class Player(Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((50, 50))
+        self.image = pygame.Surface((PPM, PPM))
         self.image.fill((0x88, 0x88, 0))
         self.rect = self.image.get_rect()
+
+
+class Wall(Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((PPM, PPM))
+        self.image.fill((0, 0, 0xaa))
+        self.rect = self.image.get_rect()
+        print('woo-hoo')
