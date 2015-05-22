722733344138deee62fad9c55841cd6ed0300c94
aruziell <aruziell@gmail.com>
2015-05-22 18:03:08 +0200

    add velocity and speed to Game Object
    handle new variables in update() method
    implement Player GameObject

--- pybomberman/objects.py
+++ pybomberman/objects.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.math import Vector2
 from pygame.rect import Rect
 from pygame.sprite import Sprite
 from pygame import Surface
@@ -13,13 +14,20 @@ class GameObject(Node):
         super().__init__(*args, **kwargs)
         self.shape = shape
         self.sprite = sprite
+        self.velocity = Vector2()
+
+        self.speed = PPM * 2
 
     def draw(self, canvas: Surface, offset=(0, 0)):
         dest = Rect(self.sprite.rect).move(offset + self.position)
         canvas.blit(self.sprite.image, dest)
 
     def update(self, dt):
-        pass
+        if self.velocity == (0, 0):
+            return
+
+        self.velocity.normalize_ip()
+        self.position += self.velocity * self.speed * dt
 
 
 class PlayerSprite(Sprite):
@@ -28,6 +36,12 @@ class PlayerSprite(Sprite):
         self.image = pygame.Surface((PPM, PPM))
         self.image.fill((0x88, 0x88, 0))
         self.rect = self.image.get_rect()
+
+
+class Player(GameObject):
+    def __init__(self, sprite=PlayerSprite(), *args, **kwargs):
+        shape = Rectangle(0, 0, PPM >> 1, PPM >> 1)
+        super().__init__(shape, sprite, *args, **kwargs)
 
 
 class WallSprite(Sprite):
