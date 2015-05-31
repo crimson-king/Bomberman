3cb61dea54686504cd64eba6a3023bc765881fe2
aruziell <aruziell@gmail.com>
2015-05-31 12:34:33 +0200

    start pybomberman in init module

--- pybomberman/__init__.py
+++ pybomberman/__init__.py
@@ -1,2 +1,15 @@
-"""Actual game, objects, options etc."""
+"""Init module containing methods to run pybomberman with ease."""
+
 PPM = 50
+
+
+def main():
+    """Starts pybomberman."""
+    from framework.core import Game
+    from framework.state import StateGameHandler
+    from framework import state_manager
+    from pybomberman.gamestate import GameState
+    from pybomberman.config import config
+
+    state_manager.push(GameState())
+    Game(StateGameHandler(), 60, *config.resolution).start()
