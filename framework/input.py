2e1271a18af2a754dd47ffca6523163c80ba4d4d
aruziell <aruziell@gmail.com>
2015-06-04 21:55:34 +0200

    make input pass pylint 10/10

--- framework/input.py
+++ framework/input.py
@@ -1,82 +1,109 @@
+"""
+Contains classes related to processing input events like Action or
+InputManager.
+"""
+
 from enum import Enum, unique
 
+# pygame modules are loaded dynamically, thus, supress no name/members errors
+# pylint: disable=no-name-in-module
+# pylint: disable=no-member
 import pygame
 
 
 class Action:
     """Pseudo-abstract class for action"""
+
     def __bool__(self):
         return self.active()
 
-    def active(self):
+    def active(self) -> bool:
         """Is it active?"""
         raise NotImplementedError
 
     def press(self):
-        """Pressing"""
+        """Invoked on key down event"""
         pass
 
     def release(self):
-        """Releasing"""
+        """Invoked on key up event"""
         pass
 
     def reset(self):
-        """Resetting"""
+        """Resets action to initial state"""
         pass
 
 
 class NormalAction(Action):
+    """Action that is active as long as key is pressed"""
+
     @unique
     class State(Enum):
+        """Internal state enum"""
         released = 0
         pressed = 1
 
     def __init__(self):
         self.state = NormalAction.State.released
 
-    def active(self):
+    def active(self) -> bool:
+        """True if its active, False otherwise"""
         return self.state is NormalAction.State.pressed
 
     def press(self):
+        """Invoked on key down event"""
         self.state = NormalAction.State.pressed
 
     def release(self):
+        """Invoked on key up event"""
         self.state = NormalAction.State.released
 
     def reset(self):
+        """Resets action to initial state"""
         self.state = NormalAction.State.released
 
 
 class InitialAction(Action):
+    """Action active only once per press"""
+
     @unique
     class State(Enum):
+        """Internal state enum"""
         inactive = 0
         active = 1
 
     def __init__(self):
         self.state = InitialAction.State.inactive
 
-    def active(self):
+    def active(self) -> bool:
+        """True if its active, False otherwise"""
         if self.state is InitialAction.State.active:
             self.state = InitialAction.State.inactive
             return True
         return False
 
     def press(self):
+        """Invoked on key down event"""
         self.state = InitialAction.State.active
 
     def reset(self):
+        """Resets action to initial state"""
         self.state = InitialAction.State.inactive
 
 
 class InputManager:
+    """Manages Actions mapped with map_action to given key codes"""
+
     def __init__(self):
         self._action_map = {}
 
     def map_action(self, key_code, action: Action):
+        """Maps given action to given key code"""
         self._action_map[key_code] = action
 
     def handle_input(self, event):
+        """Invokes mapped actions press() and release() methods"""
+
         if not hasattr(event, 'key'):
             return
 
@@ -91,11 +118,15 @@ class InputManager:
             action.release()
 
     def reset(self):
+        """Resets all mapped actions"""
         for action in self._action_map.values():
             action.reset()
 
     def clear(self):
+        """Clears action map"""
         self._action_map.clear()
 
 
+# this is not a constant
+# pylint: disable=invalid-name
 manager = InputManager()
