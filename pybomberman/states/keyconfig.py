7c76f35b6b1349e96749b69187f08a350988d93a
aruziell <aruziell@gmail.com>
2015-06-06 17:25:05 +0200

    reformat keyconfig.py, implement action key assignment

--- pybomberman/states/keyconfig.py
+++ pybomberman/states/keyconfig.py
@@ -2,16 +2,13 @@
 This module implements functionality that allows user to view and change key
  mappings.
 """
-__author__ = 'Tomasz Rzepka'
+
+# pylint: disable=no-member
 import pygame
 
 from framework.ui import StageState, Button
-from pybomberman.menu import Item
 from pybomberman.config import config
-from framework import state_manager
-
-# pylint: disable=no-member
-pygame.init()
+from framework import state_manager, input_manager
 
 
 class KeyConfigState(StageState):
@@ -19,43 +16,51 @@ class KeyConfigState(StageState):
     This class implements State that allows user to view and change key
     mappings
     """
+
     def __init__(self):
         super().__init__()
         self.current_player = 0
         self.selecting_key = False
 
-        self.player_button = Button('PLAYER <%d> KEYS' % (self.current_player + 1))
+        self.player_button = Button(
+            'PLAYER <{}> KEYS'.format(self.current_player + 1))
         self.player_button.on_click = self.on_click
         self.stage.add_node(self.player_button)
 
-        self.action_button = Button('ACTION: ' + pygame.key.name(
-                                    config.players[self.current_player].key_binding.action))
+        self.action_button = Button(
+            'ACTION: ' + pygame.key.name(
+                config.players[self.current_player].key_binding.action))
         self.stage.add_node(self.action_button)
         self.action_button.on_click = self.on_click
 
         self.up_button = Button('UP: ' + pygame.key.name(
-                                config.players[self.current_player].key_binding.upward))
+            config.players[self.current_player].key_binding.upward))
         self.stage.add_node(self.up_button)
         self.up_button.on_click = self.on_click
 
-        self.down_button = Button('DOWN: ' + pygame.key.name(
-                                  config.players[self.current_player].key_binding.down))
+        self.down_button = Button(
+            'DOWN: ' + pygame.key.name(
+                config.players[self.current_player].key_binding.down))
         self.stage.add_node(self.down_button)
         self.down_button.on_click = self.on_click
 
-        self.left_button = Button('LEFT: ' + pygame.key.name(
-                                  config.players[self.current_player].key_binding.left))
+        self.left_button = Button(
+            'LEFT: ' + pygame.key.name(
+                config.players[self.current_player].key_binding.left))
         self.stage.add_node(self.left_button)
         self.left_button.on_click = self.on_click
 
-        self.right_button = Button('RIGHT: ' + pygame.key.name(
-                                   config.players[self.current_player].key_binding.right))
+        self.right_button = Button(
+            'RIGHT: ' + pygame.key.name(
+                config.players[self.current_player].key_binding.right))
         self.stage.add_node(self.right_button)
         self.right_button.on_click = self.on_click
 
         self.exit_button = Button('GO BACK')
         self.stage.add_node(self.exit_button)
         self.exit_button.on_click = self.on_click
+
+        self.action_to_assign = None
 
     def select_key(self):
         """ Prepares key for new mapping """
@@ -105,9 +110,8 @@ class KeyConfigState(StageState):
             self.change_current_player()
             self.update_keys()
         elif button is self.action_button:
-            self.update_keys()
-            pass
-            # config.players[self.current_player].key_binding.action = key
+            self.action_to_assign = 'action'
+            self.action_button.text = 'PRESS KEY TO ASSIGN'
         elif button is self.up_button:
             self.update_keys()
             pass
@@ -126,3 +130,11 @@ class KeyConfigState(StageState):
             # config.players[self.current_player].key_binding.right = key
         elif button is self.exit_button:
             state_manager.pop()
+
+    def handle_input(self, event):
+        if self.action_to_assign and event.type == pygame.KEYDOWN:
+            config.players[self.current_player].key_binding.action = event.key
+            self.action_to_assign = None
+            self.update_keys()
+        else:
+            input_manager.handle_input(event)
